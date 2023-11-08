afb6eabb12f5284f55a905b56b4dcacdd73fe612
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-11-08 18:07:27 +0600

    Added: used popins and vollokorn font and set colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				Popins: ['Poppins', 'sans-serif'],
+				Vollokorn: ['Vollkorn', 'serif'],
+			},
+			colors: {
+				Primary: '#8fb4ff',
+				Secondary: '#d8e4fe',
+				Accent: '#f89c6c',
+				Bg: '#f2f2f2',
+				'Clr-base': '#808080',
+			},
+		},
 	},
 	plugins: [],
 };
