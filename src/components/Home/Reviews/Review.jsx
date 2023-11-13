004980dc98adfb4b76da6bc1829047f57bee93e8
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-11-14 04:11:38 +0600

    Updated: completed the Reviews design for mobile devices

--- src/components/Home/Reviews/Review.jsx
+++ src/components/Home/Reviews/Review.jsx
@@ -1,3 +1,26 @@
+import { modelImg } from '../../../assets';
+import { Rating } from '../../../shared';
+
 export default function Review({ count }) {
-	return <div className='review h-96 w-96 bg-blue-300'>{count}</div>;
+	const feedback =
+		'"I made a delicious beef stir fry for dinner and it was amazing! The beef was tender and the vegetables were perfectly cooked."';
+	const rating = 5;
+	const name = 'Naiem';
+
+	return (
+		<div className='review h-72 w-[88vw] relative'>
+			<img src={modelImg} alt='' className='w-full h-full object-cover' />
+			<div className='w-full h-full flex justify-center items-end text-white bg-gradient-to-t from-black to-transparent to-80% absolute inset-0'>
+				<div className='text-center text-xs mb-5 px-3'>
+					<h3 className='font-Popins'>{`"${feedback}"`}</h3>
+					<p className='text-sm font-Vollokorn font-semibold mt-1'>
+						- {name}
+					</p>
+					<p className='w-fit text-yellow-300 md:text-lg flex items-center gap-[2px] mx-auto mt-1'>
+						<Rating rating={rating} />
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 }
