ed18bb065b4e15b7193ac1e48d9bdbfde6107359
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-11-17 20:42:12 +0600

    Fixed: fixed recurrence fade in animation of FavoriteRecipe component

--- src/components/Dashboard/FavoriteRecipe.jsx
+++ src/components/Dashboard/FavoriteRecipe.jsx
@@ -15,6 +15,7 @@ export default function FavoriteRecipe() {
 					hide: { opacity: 0 },
 					show: { opacity: 1 },
 				}}
+				viewport={{ once: true }}
 				transition={{ when: 'beforeChildren', delayChildren: 0.5 }}
 				className='w-11/12 mx-auto mt-8 grid grid-cols-1 lg:grid-cols-2 gap-3'>
 				{[1, 2, 3, 4, 5].map((el, index) => (
