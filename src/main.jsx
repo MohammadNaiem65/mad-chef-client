eb727435d1e0e75c4c3f257f575c025c5712b14f
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-11-19 10:34:55 +0600

    Added: created basic redux store and apiSlice

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 
 import { RouterProvider } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+import { Provider } from 'react-redux';
 import routes from './routes/routes.jsx';
+import store from './app/store.js';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<HelmetProvider>
-			<RouterProvider router={routes} />
-		</HelmetProvider>
+		<Provider store={store}>
+			<HelmetProvider>
+				<RouterProvider router={routes} />
+			</HelmetProvider>
+		</Provider>
 	</React.StrictMode>
 );
