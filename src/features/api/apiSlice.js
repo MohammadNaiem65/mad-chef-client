a64d33951aabeb837d1f60bcb496e2b4b6f9fa7a
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-12-28 17:46:57 +0600

    Updated: updated baseQuery of apiSlice to send authenticated requests

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -4,6 +4,16 @@ const apiSlice = createApi({
 	reducerPath: 'api',
 	baseQuery: fetchBaseQuery({
 		baseUrl: 'http://localhost:5000',
+		prepareHeaders: async (headers, { getState }) => {
+			const token = getState()?.auth?.accessToken;
+
+			// set authorization header
+			if (token) {
+				headers.set('Authorization', `Bearer ${token}`);
+			}
+
+			return headers;
+		},
 	}),
 	// eslint-disable-next-line no-unused-vars
 	endpoints: (builder) => ({}),
