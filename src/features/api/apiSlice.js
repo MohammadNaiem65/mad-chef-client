4a3ebfbd0f5dc14adc8c91a0908c4bf8c78ce98c
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-01-12 16:01:51 +0600

    Updated: customized baseQuery for refetching after accessToken expiration

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,20 +1,64 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { removeCredentials, setCredentials } from '../auth/authSlice';
+
+const baseQuery = fetchBaseQuery({
+	baseUrl: 'http://localhost:5000',
+	credentials: 'include',
+	prepareHeaders: async (headers, { getState }) => {
+		const token = getState()?.auth?.accessToken;
+
+		// set authorization header
+		if (token) {
+			headers.set('Authorization', `Bearer ${token}`);
+		}
+
+		return headers;
+	},
+});
 
 const apiSlice = createApi({
 	reducerPath: 'api',
-	baseQuery: fetchBaseQuery({
-		baseUrl: 'http://localhost:5000',
-		prepareHeaders: async (headers, { getState }) => {
-			const token = getState()?.auth?.accessToken;
+	baseQuery: async (args, api, extraOptions) => {
+		let result = await baseQuery(args, api, extraOptions);
 
-			// set authorization header
-			if (token) {
-				headers.set('Authorization', `Bearer ${token}`);
+		if (
+			result?.error?.status === 403 &&
+			result?.error?.data?.data === 'jwt expired'
+		) {
+			// refetch for new access token
+			const refreshResult = await baseQuery(
+				'/auth/refresh-token',
+				api,
+				extraOptions
+			);
+
+			if (refreshResult?.data) {
+				const user = api.getState().auth.user;
+
+				const data = {
+					user,
+					accessToken: refreshResult.data?.data?.accessToken,
+				};
+
+				// update the access token
+				localStorage.setItem('auth', JSON.stringify(data));
+				api.dispatch(setCredentials(data));
+
+				// fetch the previous request
+				result = await baseQuery(args, api, extraOptions);
+			} else if (
+				refreshResult?.error?.status === 403 &&
+				refreshResult?.error?.data?.data === 'jwt expired'
+			) {
+				localStorage.removeItem('auth');
+				api.dispatch(removeCredentials());
 			}
+		}
 
-			return headers;
-		},
-	}),
+		// console.log(result);
+
+		return result;
+	},
 	// eslint-disable-next-line no-unused-vars
 	endpoints: (builder) => ({}),
 });
