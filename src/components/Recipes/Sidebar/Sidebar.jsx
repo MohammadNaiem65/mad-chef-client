a94c6b504e216c2aa09c64b4c9e3cec93f10e71e
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-04-02 10:01:46 +0600

    Edited: redesigned the sidebar

--- src/components/Recipes/Sidebar/Sidebar.jsx
+++ src/components/Recipes/Sidebar/Sidebar.jsx
@@ -39,12 +39,17 @@ export default function Sidebar() {
 			</p>
 
 			<aside
-				className={`h-screen lg:max-h-[35rem] w-full md:w-2/3 lg:w-[25rem] px-3 py-5 bg-Primary/20 backdrop-blur-lg lg:rounded fixed lg:sticky top-0 lg:top-20 duration-300 z-10 ${
+				className={`h-screen lg:max-h-[35rem] w-full md:w-2/3 lg:w-[25rem] px-5 py-5 bg-Primary/20 backdrop-blur-lg lg:rounded fixed lg:sticky top-0 lg:top-20 duration-300 z-10 ${
 					showBar
 						? 'left-0 md:left-1/2 lg:left-0 md:-translate-x-1/2 lg:translate-x-0'
 						: '-left-[100%]'
 				}`}>
-				<div className='sidebar h-full px-2 overflow-y-scroll'>
+				<input
+					type='text'
+					id=''
+					className='sidebar w-full mb-4 px-2 py-1 text-gray-700 font-semibold outline-Primary rounded'
+				/>
+				<div className='sidebar h-[91%] overflow-y-scroll'>
 					{chefs.map((chef, index) => (
 						<NavLink
 							key={index}
