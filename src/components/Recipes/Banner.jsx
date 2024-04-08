e5cde789d3caaabe7422010fe0102b31940cda5a
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-04-08 15:43:31 +0600

    update: made the Banner of Recipes page dynamic

--- src/components/Recipes/Banner.jsx
+++ src/components/Recipes/Banner.jsx
@@ -1,23 +1,23 @@
+import { useParams } from 'react-router-dom';
 import { FaBriefcase, FaStar } from 'react-icons/fa6';
 import { MdFastfood } from 'react-icons/md';
-import { modelImg } from '../../assets';
+import { useGetChefQuery } from '../../features/chef/chefApi';
 
 export default function Banner() {
-	const name = 'Mohammad Naiem';
-	const rating = 5;
-	const yearsOfExperience = 3;
-	const availableRecipes = [2, 3, 5, 3, 5];
-	const bio =
-		'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta ut placeat ea nulla cumque impedit consequatur sit ullam dolorum. Voluptatibus ad aliquam maxime nostrum non aspernatur dolorum similique dolorem consequatur.';
+	const { chefId } = useParams();
+
+	const { data } = useGetChefQuery({ chefId });
+	const { name, bio, rating, yearsOfExperience, recipes, img } =
+		data?.data || {};
 
 	return (
-		<section className='mb-10 p-6 bg-Primary/20 text-gray-500 font-Popins lg:flex gap-x-5 rounded'>
+		<section className='mb-10 p-6 bg-Primary/20 text-gray-500 font-Popins lg:flex gap-x-5 flex justify-between rounded'>
 			<img
-				className='h-44 lg:h-52 aspect-square mx-auto rounded-full object-cover'
-				src={modelImg}
+				className='h-44 lg:h-52 aspect-square rounded-full object-cover'
+				src={img}
 				alt={`Chef ${name}'s picture`}
 			/>
-			<div className='h-fit my-auto text-justify'>
+			<div className='w-full h-fit my-auto text-justify'>
 				<p className='mt-2 lg:mb-3 text-2xl text-black text-center lg:text-start font-Vollokorn font-semibold'>
 					{name}
 				</p>
@@ -27,19 +27,21 @@ export default function Banner() {
 						<span className='mr-1 text-yellow-500'>
 							<FaStar />
 						</span>
-						{rating}
+						{rating ? rating : 'Null'}
 					</p>
 					<p className='flex items-center'>
 						<span className='mr-1'>
 							<FaBriefcase />
 						</span>
-						{yearsOfExperience} years
+						{yearsOfExperience
+							? `${yearsOfExperience} years`
+							: 'Experience unavailable'}
 					</p>
 					<p className='flex items-center'>
 						<span className='mr-1'>
 							<MdFastfood />
 						</span>
-						{availableRecipes.length} recipes
+						{recipes?.length} recipes
 					</p>
 				</div>
 			</div>
