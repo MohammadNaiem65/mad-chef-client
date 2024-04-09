1b061ea9fdf885a49694462d78f65c26f081cbf2
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-04-09 16:51:07 +0600

    add: added Pagination

--- src/components/Recipes/ChefRecipes.jsx
+++ src/components/Recipes/ChefRecipes.jsx
@@ -3,6 +3,7 @@ import { FaAngleLeft, FaAngleRight } from 'react-icons/fa6';
 
 import { useGetRecipesQuery } from '../../features/recipe/recipeApi';
 import Recipe from '../../shared/Recipe';
+import { Pagination } from '../../shared';
 
 export default function ChefRecipes({ chefId }) {
 	const [pageDetails, setPageDetails] = useState({
@@ -30,43 +31,58 @@ export default function ChefRecipes({ chefId }) {
 
 	return (
 		<section className='w-full px-5 lg:p-0'>
-			<div className='flex justify-between items-center'>
-				<h3 className='text-xl font-semibold font-Vollokorn'>
-					Recipes:
-				</h3>
-				<div className='text-3xl text-Primary flex items-center gap-x-2'>
-					<button
-						disabled={pageDetails.currPage === 1}
-						className='disabled:translate-y-1 disabled:scale-95 duration-300 disabled:text-blue-700 translate-y-0'
-						onClick={() =>
-							setPageDetails((prev) => ({
-								...prev,
-								currPage: prev.currPage - 1,
-							}))
-						}>
-						<FaAngleLeft className='cursor-pointer' />
-					</button>
-					<button
-						disabled={
-							pageDetails.currPage === pageDetails.totalPages
-						}
-						className='disabled:translate-y-1 disabled:scale-95 duration-300 disabled:text-blue-700 translate-y-0'
-						onClick={() =>
-							setPageDetails((prev) => ({
-								...prev,
-								currPage: prev.currPage + 1,
-							}))
-						}>
-						<FaAngleRight className='cursor-pointer' />
-					</button>
+			{!recipes?.length ? (
+				<p className='h-full mt-36 text-slate-700 text-center font-semibold'>
+					No Content Found
+				</p>
+			) : (
+				<div className='flex justify-between items-center'>
+					<h3 className='text-xl font-semibold font-Vollokorn'>
+						Recipes:
+					</h3>
+					<div className='text-3xl text-Primary flex items-center gap-x-2'>
+						<button
+							disabled={pageDetails.currPage === 1}
+							className='disabled:translate-y-1 disabled:scale-95 duration-300 disabled:text-blue-700 translate-y-0'
+							onClick={() =>
+								setPageDetails((prev) => ({
+									...prev,
+									currPage: prev.currPage - 1,
+								}))
+							}>
+							<FaAngleLeft className='cursor-pointer' />
+						</button>
+						<button
+							disabled={
+								pageDetails.currPage ===
+									pageDetails.totalPages ||
+								pageDetails.currPage === null
+							}
+							className='disabled:translate-y-1 disabled:scale-95 duration-300 disabled:text-blue-700 translate-y-0'
+							onClick={() =>
+								setPageDetails((prev) => ({
+									...prev,
+									currPage: prev.currPage + 1,
+								}))
+							}>
+							<FaAngleRight className='cursor-pointer' />
+						</button>
+					</div>
 				</div>
-			</div>
+			)}
 
 			<section className='mt-4'>
 				{recipes?.map((recipe, index) => (
 					<Recipe key={recipe._id} index={index} recipe={recipe} />
 				))}
 			</section>
+
+			{recipes?.length && (
+				<Pagination
+					activePage={pageDetails.currPage}
+					totalPages={pageDetails.totalPages}
+				/>
+			)}
 		</section>
 	);
 }
