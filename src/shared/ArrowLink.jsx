5dc0f275738a2a4942b713ccabdbd082b263f3aa
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-04-18 01:06:32 +0600

    update: wrapped ArrowLink component with Link element for direct redirection using ArrowLink component

--- src/shared/ArrowLink.jsx
+++ src/shared/ArrowLink.jsx
@@ -1,6 +1,7 @@
+import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
-export default function ArrowLink() {
+export default function ArrowLink({ to }) {
 	const arrowVariant = {
 		hover: {
 			x: [-20, 0],
@@ -10,26 +11,28 @@ export default function ArrowLink() {
 	};
 
 	return (
-		<motion.p
-			whileHover='hover'
-			whileTap='hover'
-			className='w-12 h-6 flex justify-center items-center'>
-			{[0, 1, 2].map((time) => (
-				<motion.span
-					key={time}
-					initial={{
-						x: 0,
-						opacity: 1,
-						rotate: '45deg',
-					}}
-					variants={arrowVariant}
-					transition={{
-						duration: 0.7,
-						delay: 0.25 * time,
-					}}
-					className='w-3 h-3 border-t-[3px] border-r-[3px] border-Primary rounded-tr-[2px] block transform rotate-45'
-				/>
-			))}
-		</motion.p>
+		<Link to={to}>
+			<motion.p
+				whileHover='hover'
+				whileTap='hover'
+				className='w-12 h-6 flex justify-center items-center'>
+				{[0, 1, 2].map((time) => (
+					<motion.span
+						key={time}
+						initial={{
+							x: 0,
+							opacity: 1,
+							rotate: '45deg',
+						}}
+						variants={arrowVariant}
+						transition={{
+							duration: 0.7,
+							delay: 0.25 * time,
+						}}
+						className='w-3 h-3 border-t-[3px] border-r-[3px] border-Primary rounded-tr-[2px] block transform rotate-45'
+					/>
+				))}
+			</motion.p>
+		</Link>
 	);
 }
