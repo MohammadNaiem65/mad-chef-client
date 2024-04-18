8a9cdb6b3a9164a4c7221e526a6ab6b5a438d144
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-04-18 22:09:34 +0600

    update: made the Review section dynamic

--- src/components/Home/Reviews/Review.jsx
+++ src/components/Home/Reviews/Review.jsx
@@ -1,20 +1,21 @@
 import { modelImg } from '../../../assets';
 import { Rating } from '../../../shared';
 
-export default function Review({ count }) {
-	const feedback =
-		'"I made a delicious beef stir fry for dinner and it was amazing! The beef was tender and the vegetables were perfectly cooked."';
-	const rating = 5;
-	const name = 'Naiem';
+export default function Review({ review }) {
+	const { rating, message, username, userImg } = review || {};
 
 	return (
-		<div className='h-72 md:h-80 w-[88vw] md:w-96 relative roundeds overflow-hidden'>
-			<img src={modelImg} alt='' className='w-full h-full object-cover' />
+		<div className='h-72 md:h-80 w-[88vw] md:w-96 relative rounded overflow-hidden'>
+			<img
+				src={userImg ? userImg : modelImg}
+				alt=''
+				className='w-full h-full object-cover'
+			/>
 			<div className='w-full h-full flex justify-center items-end text-white bg-gradient-to-t from-black to-transparent to-80% absolute inset-0'>
 				<div className='text-center text-xs mb-5 px-3'>
-					<h3 className='font-Popins'>{`"${feedback}"`}</h3>
+					<h3 className='font-Popins'>{`"${message}"`}</h3>
 					<p className='text-sm font-Vollokorn font-semibold mt-1'>
-						- {name}
+						- {username}
 					</p>
 					<p className='w-fit text-yellow-300 md:text-lg flex items-center gap-[2px] mx-auto mt-1'>
 						<Rating rating={rating} />
--- src/components/Home/Reviews/Reviews.jsx
+++ src/components/Home/Reviews/Reviews.jsx
@@ -1,9 +1,14 @@
+import { useGetRecipeRatingsQuery } from '../../../features/recipe/recipeApi';
 import Review from './Review';
 import './Reviews.css';
 
-const reviews = [1, 2, 3, 4, 5];
-
 export default function Reviews() {
+	const { data } = useGetRecipeRatingsQuery({
+		limit: 6,
+		include: 'username,userImg,rating,message',
+	});
+	const reviews = data?.data || [];
+
 	return (
 		<section className='mt-20 lg:mx-auto'>
 			<h2 className='section-title'>
@@ -15,9 +20,11 @@ export default function Reviews() {
 
 			<div className='outer-container mt-8 overflow-hidden'>
 				<div className='inner-container w-fit flex gap-5 md:gap-3'>
-					{[...reviews, ...reviews].map((review, index) => (
-						<Review key={index} count={review} />
-					))}
+					{[...reviews, ...reviews, ...reviews, ...reviews].map(
+						(review) => (
+							<Review key={review?._id} review={review} />
+						)
+					)}
 				</div>
 			</div>
 		</section>
