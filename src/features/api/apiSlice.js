5a45d41ccc455bc376f9a520fc824756732ff672
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-04-28 17:18:42 +0600

    fix: used async-mutex package to handle multiple requests of refresh token

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,16 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { removeCredentials, setCredentials } from '../auth/authSlice';
+import { Mutex } from 'async-mutex';
 
-// Flag to track if a refresh token request is in progress
-let isRefreshing = false;
+// Create a new mutex
+const mutex = new Mutex();
 
-// Define the base query function
 const baseQuery = fetchBaseQuery({
 	baseUrl: 'http://localhost:5000',
 	prepareHeaders: async (headers, { getState }) => {
 		const token = getState()?.auth?.accessToken;
 
-		// If there's an access token, add it to the request headers
+		// Set authorization header
 		if (token) {
 			headers.set('Authorization', `Bearer ${token}`);
 		}
@@ -20,47 +20,55 @@ const baseQuery = fetchBaseQuery({
 	credentials: 'include',
 });
 
-// Define the API slice
 const apiSlice = createApi({
 	reducerPath: 'api',
 	baseQuery: async (args, api, extraOptions) => {
 		let result = await baseQuery(args, api, extraOptions);
 
-		// Check if the response indicates an expired token
 		if (
 			result?.error?.status === 403 &&
 			result?.error?.data?.data === 'jwt expired'
 		) {
-			// Check if a refresh is already in progress
-			if (!isRefreshing) {
-				isRefreshing = true;
+			// Wait until the mutex is available without locking it
+			await mutex.waitForUnlock();
 
-				// Attempt to refresh the token
-				const refreshResult = await baseQuery(
-					'/auth/refresh-token',
-					api,
-					extraOptions
-				);
+			// Check if the mutex is locked
+			if (!mutex.isLocked()) {
+				const release = await mutex.acquire();
+				try {
+					// Refetch for new access token
+					const refreshResult = await baseQuery(
+						'/auth/refresh-token',
+						api,
+						extraOptions
+					);
 
-				if (refreshResult?.data) {
-					// If successful, update the access token in local storage and Redux state
-					const user = api.getState().auth.user;
-					const data = {
-						user,
-						accessToken: refreshResult.data?.data?.accessToken,
-					};
-					localStorage.setItem('auth', JSON.stringify(data));
-					api.dispatch(setCredentials(data));
+					if (refreshResult?.data) {
+						const user = api.getState().auth.user;
 
-					// Retry the original request with the new token
-					result = await baseQuery(args, api, extraOptions);
-				} else {
-					// If refresh fails, remove credentials and handle accordingly
-					localStorage.removeItem('auth');
-					api.dispatch(removeCredentials());
+						const data = {
+							user,
+							accessToken: refreshResult.data?.data?.accessToken,
+						};
+
+						// Update the access token
+						localStorage.setItem('auth', JSON.stringify(data));
+						api.dispatch(setCredentials(data));
+
+						// Fetch the previous request
+						result = await baseQuery(args, api, extraOptions);
+					} else {
+						localStorage.removeItem('auth');
+						api.dispatch(removeCredentials());
+					}
+				} finally {
+					// Release must be called once the mutex should be released again.
+					release();
 				}
-
-				isRefreshing = false;
+			} else {
+				// Wait until the mutex is available without locking it
+				await mutex.waitForUnlock();
+				result = await baseQuery(args, api, extraOptions);
 			}
 		}
 
