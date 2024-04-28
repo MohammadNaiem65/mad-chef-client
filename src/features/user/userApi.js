541feefce03abde54171f4e6c7cee22b5389349a
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-04-28 17:25:11 +0600

    update: added filter features in getUserData query

--- src/features/user/userApi.js
+++ src/features/user/userApi.js
@@ -3,9 +3,32 @@ import apiSlice from '../api/apiSlice';
 const userApi = apiSlice.injectEndpoints({
 	endpoints: (builder) => ({
 		getUserData: builder.query({
-			query: (data) => ({
-				url: `/users/${data.userId}`,
-			}),
+			query: ({ userId, include, exclude }) => {
+				// Define the base URL
+				const baseUrl = `/users/user/${userId}`;
+
+				// Create an object with all parameters
+				const params = { include, exclude };
+
+				// Use reduce to construct the query string without filter parameter
+				const queryString = Object.entries(params)
+					.reduce((acc, [key, value]) => {
+						// Only include parameters that have a value
+						if (value) {
+							acc.push(`${key}=${encodeURIComponent(value)}`);
+						}
+						return acc;
+					}, [])
+					.join('&');
+
+				let finalUrl = baseUrl;
+
+				if (queryString) {
+					finalUrl += `?${queryString}`;
+				}
+
+				return { url: finalUrl };
+			},
 		}),
 	}),
 });
