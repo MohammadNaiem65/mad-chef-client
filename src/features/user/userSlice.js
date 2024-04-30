73d2ce9d989a3b2c4e46d28c1be8326d2d0ad665
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-04-30 12:18:51 +0600

    update: made the initialState empty object to store every user data rather than just student

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,36 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-	_id: null,
-	name: null,
-	email: null,
-	emailVerified: false,
-	role: null,
-	pkg: 'basic',
-	img: null,
-};
+const initialState = {};
 
 const userSlice = createSlice({
 	name: 'user',
 	initialState,
 	reducers: {
 		addUserData: (state, action) => {
-			state._id = action.payload?._id;
-			state.name = action.payload.name;
-			state.email = action.payload.email;
-			state.emailVerified = action.payload.emailVerified;
-			state.role = action.payload.role;
-			state.pkg = action.payload.pkg;
-			state.img = action.payload.img;
+			Object.assign(state, action.payload);
 		},
 		removeUserData: (state) => {
-			state._id = null;
-			state.name = null;
-			state.email = null;
-			state.emailVerified = false;
-			state.role = null;
-			state.pkg = 'basic';
-			state.img = null;
+			Object.assign(state, {});
 		},
 	},
 });
