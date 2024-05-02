4c047b4cad9c29613789c6935ce3d0e44b3004e8
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-05-02 14:19:37 +0600

    update: added optimistically cache update functionality after editing or deleting rating and review documents

--- src/features/user/userApi.js
+++ src/features/user/userApi.js
@@ -1,3 +1,4 @@
+import showNotification from '../../helpers/showNotification';
 import apiSlice from '../api/apiSlice';
 
 const userApi = apiSlice.injectEndpoints({
@@ -30,21 +31,84 @@ const userApi = apiSlice.injectEndpoints({
 				return { url: finalUrl };
 			},
 		}),
-		getChefReviewsByUser: builder.query({
-			query: ({ userId }) => `/users/user/${userId}/review/chef`,
-		}),
 		editRecipeRatingByUser: builder.mutation({
 			query: ({ userId, docId, data }) => ({
 				url: `/users/user/${userId}/rating/recipe?docId=${docId}`,
 				method: 'PATCH',
 				body: data,
 			}),
+			async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+				const { userId, docId, data } = arg;
+
+				// Optimistically update the recipe rating
+				const updatePatchResult = dispatch(
+					apiSlice.util.updateQueryData(
+						'getRecipeRatings',
+						{ data_filter: { userId } },
+						(draft) => {
+							const ratings = draft?.data;
+
+							const recipeToEdit = ratings.find(
+								(rating) => rating?._id === docId
+							);
+
+							recipeToEdit.rating = data.rating;
+							recipeToEdit.message = data.message;
+						}
+					)
+				);
+
+				try {
+					await queryFulfilled;
+				} catch (error) {
+					// Revert the optimistic patch updates and notify the user
+					updatePatchResult.undo();
+					showNotification(
+						'error',
+						'An error occurred while updating the rating.'
+					);
+				}
+			},
 		}),
 		deleteRecipeRatingByUser: builder.mutation({
 			query: ({ userId, docId }) => ({
 				url: `/users/user/${userId}/rating/recipe?docId=${docId}`,
 				method: 'DELETE',
 			}),
+			async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+				const { userId, docId } = arg;
+
+				// Optimistically delete the chef review document
+				const deletePatchResult = dispatch(
+					apiSlice.util.updateQueryData(
+						'getRecipeRatings',
+						{ data_filter: { userId } },
+						(draft) => {
+							const documents = draft?.data;
+
+							const docsWithoutDeletedOne = documents?.filter(
+								(doc) => doc._id !== docId
+							);
+
+							draft.data = docsWithoutDeletedOne;
+						}
+					)
+				);
+
+				try {
+					await queryFulfilled;
+				} catch (error) {
+					// Revert the optimistically deleting rating document and notify the user
+					deletePatchResult.undo();
+					showNotification(
+						'error',
+						'An error occurred while deleting the rating'
+					);
+				}
+			},
+		}),
+		getChefReviewsByUser: builder.query({
+			query: ({ userId }) => `/users/user/${userId}/review/chef`,
 		}),
 		editChefReviewsByUser: builder.mutation({
 			query: ({ userId, docId, data }) => ({
@@ -52,12 +116,75 @@ const userApi = apiSlice.injectEndpoints({
 				method: 'PATCH',
 				body: data,
 			}),
+			async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+				const { userId, docId, data } = arg;
+
+				// Optimistically update the recipe rating
+				const updatePatchResult = dispatch(
+					apiSlice.util.updateQueryData(
+						'getChefReviewsByUser',
+						{ userId },
+						(draft) => {
+							const ratings = draft?.data;
+
+							const recipeToEdit = ratings.find(
+								(rating) => rating?._id === docId
+							);
+
+							recipeToEdit.rating = data.rating;
+							recipeToEdit.message = data.message;
+						}
+					)
+				);
+
+				try {
+					await queryFulfilled;
+				} catch (error) {
+					// Revert the optimistic patch updates and notify the user
+					updatePatchResult.undo();
+					showNotification(
+						'error',
+						'An error occurred while updating the review'
+					);
+				}
+			},
 		}),
 		deleteChefReviewByUser: builder.mutation({
 			query: ({ userId, docId }) => ({
 				url: `/users/user/${userId}/review/chef?docId=${docId}`,
 				method: 'DELETE',
 			}),
+			async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+				const { userId, docId } = arg;
+
+				// Optimistically delete the chef review document
+				const deletePatchResult = dispatch(
+					apiSlice.util.updateQueryData(
+						'getChefReviewsByUser',
+						{ userId },
+						(draft) => {
+							const documents = draft?.data;
+
+							const docsWithoutDeletedOne = documents?.filter(
+								(doc) => doc._id !== docId
+							);
+
+							draft.data = docsWithoutDeletedOne;
+						}
+					)
+				);
+
+				try {
+					await queryFulfilled;
+				} catch (error) {
+					// Revert the optimistically deleting review document and notify the user
+					deletePatchResult.undo();
+					showNotification(
+						'error',
+						'An error occurred while deleting the review'
+					);
+				}
+			},
 		}),
 	}),
 });
