5c3a4ed2ddc37d6a38575dda8f43fb38f514c97d
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-05-15 18:23:28 +0600

    update: completed Payment component design and made it functional

--- src/components/Profile/User/pages/PaymentHistory/Payment.jsx
+++ src/components/Profile/User/pages/PaymentHistory/Payment.jsx
@@ -1,7 +1,105 @@
-export default function Payment() {
-    return (
-        <section>
-            Payment
-        </section>
-    );
+import { BsExclamationLg } from 'react-icons/bs';
+import { FaRegClock } from 'react-icons/fa6';
+import { IoCheckmarkDone } from 'react-icons/io5';
+import { MdDeleteForever } from 'react-icons/md';
+import { useDeletePaymentReceiptMutation } from '../../../../../features/payment/paymentApi';
+
+export default function Payment(props) {
+	const { userId, filter, payment, selectedDocs, setSelectedDocs } = props;
+	const { _id, username, email, pkg, amount, status, createdAt } =
+		payment || {};
+
+	const [deleteReceipt] = useDeletePaymentReceiptMutation();
+
+	const packageName =
+		pkg?.split('/') && pkg?.split('/')[1]?.replace(/-/g, ' ');
+	const date = `${new Date(createdAt).toLocaleTimeString('en-US', {
+		hour: 'numeric',
+		minute: 'numeric',
+		hour12: true,
+	})} - ${new Date(createdAt).toLocaleDateString('en-US', {
+		day: 'numeric',
+		month: 'short',
+		year: 'numeric',
+	})}`;
+	const selected = selectedDocs.includes(_id);
+
+	const handleDeletion = () => {
+		deleteReceipt({ userId, filter, receiptId: _id });
+	};
+
+	return (
+		<div className='p-2 text-sm text-gray-500 grid grid-cols-12 group/parent'>
+			<div className='col-span-2 flex items-center gap-x-2'>
+				<input
+					type='checkbox'
+					name='transaction'
+					id='transaction'
+					className='size-4 accent-Primary'
+					checked={selected}
+					onChange={() =>
+						selected
+							? setSelectedDocs((prev) =>
+									prev.filter((id) => id !== _id)
+							)
+							: setSelectedDocs((prev) => [...prev, _id])
+					}
+				/>
+
+				{/* Title */}
+				<p className='text-base text-gray-700'>
+					${(amount / 100).toFixed(2)}
+				</p>
+
+				{/* Status */}
+				<p
+					title={`Order Status: ${status}`}
+					className={`w-fit py-[.07rem] px-2 text-xs capitalize flex items-center gap-x-1 rounded-sm ${
+						status === 'pending'
+							? 'bg-slate-300 text-slate-700'
+							: status === 'succeeded'
+							? 'py-[.09rem] bg-green-200 text-green-600'
+							: status === 'failed' && 'bg-red-200 text-red-600'
+					}`}>
+					{status}
+					{status === 'pending' ? (
+						<FaRegClock className='text-xs my-1' />
+					) : status === 'succeeded' ? (
+						<IoCheckmarkDone className='text-lg' />
+					) : (
+						status === 'failed' && (
+							<BsExclamationLg className='text-lg -mx-1 ' />
+						)
+					)}
+				</p>
+			</div>
+
+			<span title={username} className='truncate ml-3 col-span-2'>
+				{username}
+			</span>
+			<span title={email} className='truncate col-span-3'>
+				{email}
+			</span>
+			<span
+				title={packageName}
+				className='capitalize truncate col-span-2'>
+				{packageName}
+			</span>
+			<span
+				title={date}
+				className='col-span-3 truncate group-hover/parent:col-span-2'>
+				{date}
+			</span>
+
+			{/* Action buttons */}
+			<div className='col-span-1 hidden justify-center items-center group-hover/parent:flex'>
+				<button
+					title='Delete Receipt'
+					className='text-2xl'
+					onClick={handleDeletion}>
+					<MdDeleteForever className='hover:text-red-500' />
+				</button>
+			</div>
+		</div>
+	);
 }
--- src/components/Profile/User/pages/PaymentHistory/PaymentHistory.jsx
+++ src/components/Profile/User/pages/PaymentHistory/PaymentHistory.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { Helmet } from 'react-helmet-async';
 import {
 	MdOutlineFactCheck,
@@ -6,24 +8,31 @@ import {
 	MdError,
 } from 'react-icons/md';
 import { PiCardholderLight, PiCardholderFill } from 'react-icons/pi';
+
+import { useGetPaymentReceiptsQuery } from '../../../../../features/payment/paymentApi';
+import { Spinner } from '../../../../../shared';
 import Sidebar from '../../Sidebar';
 import Payment from './Payment';
-import { useState } from 'react';
 
 export default function PaymentHistory() {
 	const [filter, setFilter] = useState('all');
+	const [selectedDocs, setSelectedDocs] = useState([]);
 
-	console.log(filter);
+	const { _id } = useSelector((state) => state.user);
+	const { data, isLoading, isSuccess, isError, error } =
+		useGetPaymentReceiptsQuery({
+			userId: _id,
+			filter,
+		});
+	const receipts = data?.data || {};
 
-	return (
-		<section className='border-t border-gray-300 flex'>
-			<Helmet>
-				<title>Payment History || Profile - Mad Chef</title>
-			</Helmet>
+	// Decide what to render
+	let content;
 
-			{/* Sidebar */}
-			<Sidebar />
-
+	if (isLoading) {
+		content = <Spinner />;
+	} else if (isError) {
+		content = (
 			<div className='w-full'>
 				{/* Navbar for dashboard page content */}
 				<nav className='text-lg flex items-center divide-x-2'>
@@ -66,14 +75,112 @@ export default function PaymentHistory() {
 				</nav>
 
 				{/* Payments */}
-				<section>
-					{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map(
-						(payment, index) => (
-							<Payment key={index} payment={payment} />
-						)
-					)}
-				</section>
+				<div className='w-full mt-32 flex justify-center'>
+					<p className='w-fit h-fit p-2 bg-red-300 text-red-700 rounded-sm'>
+						{error?.message}
+					</p>
+				</div>
 			</div>
+		);
+	} else if (isSuccess && receipts?.length === 0) {
+		content = (
+			<div className='w-full mt-32 flex justify-center'>
+				<p>No data found</p>
+			</div>
+		);
+	} else if (isSuccess && receipts.length > 0) {
+		content = (
+			<section className='ml-5 divide-y-2'>
+				<div className='p-2 text-sm text-gray-500 grid grid-cols-12 group/parent'>
+					<span className='text-base text-gray-700 col-span-2'>
+						Amount
+					</span>
+					<span className='text-base text-gray-700 truncate ml-3 col-span-2'>
+						Username
+					</span>
+					<span className='text-base text-gray-700 truncate col-span-3'>
+						Email
+					</span>
+					<span className='text-base text-gray-700 capitalize truncate col-span-2'>
+						Package Name
+					</span>
+					<span className='text-base text-gray-700 col-span-3 truncate group-hover/parent:col-span-2'>
+						Date
+					</span>
+				</div>
+
+				{/* Receipts */}
+				{receipts.map((payment, index) => (
+					<Payment
+						key={index}
+						userId={_id}
+						filter={filter}
+						payment={payment}
+						selectedDocs={selectedDocs}
+						setSelectedDocs={setSelectedDocs}
+					/>
+				))}
+			</section>
+		);
+	}
+
+	return (
+		<section className='border-t border-gray-300 flex'>
+			<Helmet>
+				<title>Payment History || Profile - Mad Chef</title>
+			</Helmet>
+
+			{/* Sidebar */}
+			<Sidebar />
+
+			{/* {content} */}
+			<section className='w-full'>
+				{/* Navbar for dashboard page content */}
+				<nav className='mb-10 text-lg flex items-center divide-x-2'>
+					<button
+						className={`w-52 px-5 py-2 flex items-center gap-x-3 hover:bg-Primary/10 ${
+							filter === 'all' && 'text-Primary'
+						}`}
+						onClick={() => setFilter('all')}>
+						{filter === 'all' ? (
+							<PiCardholderFill className='text-2xl' />
+						) : (
+							<PiCardholderLight className='text-2xl' />
+						)}
+						All
+					</button>
+					<button
+						className={`w-52 px-5 py-2 flex items-center gap-x-3 hover:bg-Primary/10 ${
+							filter === 'succeeded' && 'text-Primary'
+						}`}
+						onClick={() => setFilter('succeeded')}>
+						{filter === 'succeeded' ? (
+							<MdFactCheck className='text-xl' />
+						) : (
+							<MdOutlineFactCheck className='text-xl' />
+						)}
+						Succeeded
+					</button>
+					<button
+						className={`w-52 px-5 py-2 flex items-center gap-x-3 hover:bg-Primary/10 ${
+							filter === 'failed' && 'text-Primary'
+						}`}
+						onClick={() => setFilter('failed')}>
+						{filter === 'failed' ? (
+							<MdError className='text-2xl' />
+						) : (
+							<MdErrorOutline className='text-2xl' />
+						)}
+						Failed
+					</button>
+				</nav>
+
+				<h3 className='w-1/2 ml-4 mb-4 px-2 border-b-2 text-2xl font-semibold text-slate-700 border-Primary'>
+					My Transactions:
+				</h3>
+
+				{content}
+			</section>
 		</section>
 	);
 }
