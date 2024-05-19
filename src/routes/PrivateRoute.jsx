c687a94207dd5e3d9afd75fc93f15091988a55ee
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-05-19 15:28:26 +0600

    update: used location to redirect the user to the desired route

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,14 +1,15 @@
 import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { useLocation, Navigate } from 'react-router-dom';
 import { selectAuth } from '../features/auth/authSelectors';
 
 export default function PrivateRoute({ children }) {
 	// get auth data from the redux store
 	const auth = useSelector(selectAuth);
+	const location = useLocation();
 
 	return auth?.accessToken && auth?.user ? (
 		children
 	) : (
-		<Navigate to={'/login'} />
+		<Navigate to={'/login'} state={{ from: location }} replace />
 	);
 }
