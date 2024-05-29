0c4f478d3508b876f77fcc009af96ab9bb955735
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-05-29 23:30:31 +0600

    update: documented the custom hooks

--- src/hooks/useAuthCheck.js
+++ src/hooks/useAuthCheck.js
@@ -2,29 +2,38 @@ import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { setCredentials } from '../features/auth/authSlice';
 
+/**
+ * Custom React Hook to check authentication status and set credentials.
+ * 
+ * @returns {boolean} - Returns true when authentication status is checked.
+ */
 export default function useAuthCheck() {
-	// local states
-	const [authChecked, setAuthChecked] = useState(false);
+    // Local state to track if authentication status is checked
+    const [authChecked, setAuthChecked] = useState(false);
 
-	// external imports
-	const dispatch = useDispatch();
+    // Dispatch function from react-redux to dispatch actions
+    const dispatch = useDispatch();
 
-	// check for authentication credentials in localStorage
-	useEffect(() => {
-		const localAuth = JSON.parse(localStorage.getItem('auth'));
+    // useEffect hook to check for authentication credentials in localStorage
+    useEffect(() => {
+        // Parse the 'auth' item from localStorage
+        const localAuth = JSON.parse(localStorage.getItem('auth'));
 
-		// check if user credentials is available
-		if (localAuth?.accessToken && localAuth?.user) {
-			dispatch(
-				setCredentials({
-					accessToken: localAuth.accessToken,
-					user: localAuth.user,
-				})
-			);
-		}
+        // Check if user credentials are available
+        if (localAuth?.accessToken && localAuth?.user) {
+            // Dispatch action to set the credentials in the Redux store
+            dispatch(
+                setCredentials({
+                    accessToken: localAuth.accessToken,
+                    user: localAuth.user,
+                })
+            );
+        }
 
-		setAuthChecked(true);
-	}, [dispatch]);
+        // Set authChecked to true after checking the authentication status
+        setAuthChecked(true);
+    }, [dispatch]); // Dependency on dispatch to re-run the effect when the dispatch function changes
 
-	return authChecked;
+    // Return the authChecked status
+    return authChecked;
 }
