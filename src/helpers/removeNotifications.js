ee1a69c3df27a027a8a016f05b8bdb525c895783
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-05-29 23:37:52 +0600

    update: documented the helper functions

--- src/helpers/removeNotifications.js
+++ src/helpers/removeNotifications.js
@@ -1,5 +1,12 @@
 import toast from 'react-hot-toast';
 
+/**
+ * Dismisses all active toast notifications.
+ *
+ * This function utilizes the `react-hot-toast` library to clear all currently displayed
+ * toast notifications from the screen. It does not take any parameters and does not
+ * return any values.
+ */
 export default function removeNotifications() {
 	toast.dismiss();
 }
