618aab1882829f4fb44995ab1fe70d14bfff9578
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-06-03 20:13:26 +0600

    update: made the Sidebar component responsive

--- src/components/Profile/User/Sidebar.jsx
+++ src/components/Profile/User/Sidebar.jsx
@@ -52,78 +52,89 @@ export default function Sidebar() {
 	};
 
 	return (
-		<aside className='w-1/4 py-3 border-r border-gray-300 font-Popins text-lg'>
+		<aside className='w-12 md:w-1/4 py-3 border-r border-gray-300 font-Popins text-lg'>
 			<div className='min-h-[17.25rem]'>
 				<Link
 					to='/profile/user/dashboard/'
-					className={`py-2 flex items-center gap-x-2 rounded-tl rounded-bl hover:bg-Primary/20 ${
-						mainPath === 'dashboard' ? 'bg-blue-300 px-5' : 'px-3 '
-					}`}>
+					className={`h-11 md:py-2 flex items-center gap-x-2 xl:rounded-tl xl:rounded-bl hover:bg-Primary/20 ${
+						mainPath === 'dashboard'
+							? 'bg-blue-300 md:px-5'
+							: 'md:px-3'
+					}`}
+					title='Dashboard'>
 					{mainPath === 'dashboard' ? (
-						<AiFillDashboard className='text-2xl' />
+						<AiFillDashboard className='mx-auto md:mx-0 text-3xl md:text-2xl' />
 					) : (
-						<AiOutlineDashboard className='text-2xl' />
+						<AiOutlineDashboard className='mx-auto md:mx-0 text-3xl md:text-2xl' />
 					)}
-					Dashboard
+					<span className='hidden md:inline'>Dashboard</span>
 				</Link>
 				<Link
 					to='/profile/user/messages'
-					className={`py-2 flex items-center gap-x-2 rounded-tl rounded-bl hover:bg-Primary/20 ${
+					className={`h-11 md:py-2 flex items-center gap-x-2 xl:rounded-tl xl:rounded-bl hover:bg-Primary/20 ${
 						mainPath === 'messages'
-							? 'bg-blue-300 font- px-5'
-							: 'px-3 '
-					}`}>
+							? 'bg-blue-300 md:px-5'
+							: 'md:px-3'
+					}`}
+					title='Messages'>
 					{mainPath === 'messages' ? (
-						<BiSolidMessageRoundedDetail className='text-2xl' />
+						<BiSolidMessageRoundedDetail className='mx-auto md:mx-0 text-3xl md:text-2xl' />
 					) : (
-						<BiMessageRoundedDetail className='text-2xl' />
+						<BiMessageRoundedDetail className='mx-auto md:mx-0 text-3xl md:text-2xl' />
 					)}
-					Messages
+					<span className='hidden md:inline'>Messages</span>
 				</Link>
 				<Link
 					to='/profile/user/consults'
-					className={`py-2 flex items-center gap-x-2 rounded-tl rounded-bl hover:bg-Primary/20 ${
+					className={`h-11 md:py-2 flex items-center gap-x-2 xl:rounded-tl xl:rounded-bl hover:bg-Primary/20 ${
 						mainPath === 'consults'
-							? 'bg-blue-300 font- px-5'
-							: 'px-3 '
-					}`}>
-					<BiSupport className='text-2xl' /> Consults
+							? 'bg-blue-300 md:px-5'
+							: 'md:px-3'
+					}`}
+					title='Consults'>
+					<BiSupport className='mx-auto md:mx-0 text-2xl' />{' '}
+					<span className='hidden md:inline'>Consults</span>
 				</Link>
 				<Link
 					to='/profile/user/payment-history'
-					className={`py-2 flex items-center gap-x-2 rounded-tl rounded-bl hover:bg-Primary/20 ${
+					className={`h-11 md:py-2 flex items-center gap-x-2 xl:rounded-tl xl:rounded-bl hover:bg-Primary/20 ${
 						mainPath === 'payment-history'
-							? 'bg-blue-300 font- px-5'
-							: 'px-3 '
-					}`}>
+							? 'bg-blue-300 md:px-5'
+							: 'md:px-3'
+					}`}
+					title='Payment History'>
 					{mainPath === 'payment-history' ? (
-						<BsCreditCard2FrontFill className='text-2xl' />
+						<BsCreditCard2FrontFill className='mx-auto md:mx-0 text-2xl' />
 					) : (
-						<BsCreditCard2Front className='text-2xl' />
+						<BsCreditCard2Front className='mx-auto md:mx-0 text-2xl' />
 					)}
-					Payment History
+					<span className='hidden md:inline truncate'>
+						Payment History
+					</span>
 				</Link>
 			</div>
 			<Link
 				to='/profile/user/my-profile'
-				className={`py-2 border-t border-gray-300 flex items-center gap-x-2 rounded-tl rounded-bl hover:bg-Primary/20 ${
+				className={`h-11 md:py-2 border-t border-gray-300 flex items-center gap-x-2 xl:rounded-tl xl:rounded-bl hover:bg-Primary/20 ${
 					mainPath === 'my-profile'
-						? 'bg-blue-300 font- px-5'
-						: 'px-3 '
-				}`}>
+						? 'bg-blue-300 md:px-5'
+						: 'md:px-3 '
+				}`}
+				title='My Profile'>
 				{mainPath === 'my-profile' ? (
-					<FaUserCircle className='text-2xl' />
+					<FaUserCircle className='mx-auto md:mx-0 text-2xl' />
 				) : (
-					<FaRegUserCircle className='text-2xl' />
+					<FaRegUserCircle className='mx-auto md:mx-0 text-2xl' />
 				)}
-				My Profile
+				<span className='hidden md:inline'>My Profile</span>
 			</Link>
 			<button
 				disabled={isLoading}
 				onClick={handleLogout}
-				className='w-full px-3 py-2 flex items-center gap-x-2 rounded-tl rounded-bl hover:bg-Primary/20'>
+				className='w-full h-11 px-3 py-2 flex items-center gap-x-2 rounded-tl rounded-bl hover:bg-Primary/20'
+				title='Logout'>
 				<LuLogOut className='text-2xl' />
-				Logout
+				<span className='hidden md:inline'>Logout</span>
 			</button>
 		</aside>
 	);
