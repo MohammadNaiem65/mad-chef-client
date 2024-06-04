e74c7472afe8420aaea213a4be37c3da97c8aaf7
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-06-04 23:29:52 +0600

    fix: fixed storeUserData helper to also add updatedAt field with the action

--- src/helpers/storeUserData.js
+++ src/helpers/storeUserData.js
@@ -4,40 +4,50 @@ import { addUserData } from '../features/user/userSlice';
 
 /**
  * Asynchronously fetches user data from the database and stores it in the Redux store.
- * 
+ *
  * This function performs the following steps:
  * 1. Retrieves the userId from the Redux store.
  * 2. Fetches user data from the database using the userId.
  * 3. Extracts relevant user information from the fetched data.
  * 4. Dispatches an action to save the user data in the Redux store.
- * 
+ *
  * @async
  * @function storeUserData
  * @returns {Promise<void>} A promise that resolves when the user data has been successfully stored.
  */
 export default async function storeUserData() {
-    // Get the userId from the store
-    const { userId } = store.getState().auth.user || {};
+	// Get the userId from the store
+	const { userId } = store.getState().auth.user || {};
 
-    // Get user data from database using userId
-    const { data } = await store
-        .dispatch(userApi.endpoints.getUserData.initiate({ userId }))
-        .unwrap();
+	// Get user data from database using userId
+	const { data } = await store
+		.dispatch(userApi.endpoints.getUserData.initiate({ userId }))
+		.unwrap();
 
-    const { _id, name, email, emailVerified, role, img, createdAt, pkg } =
-        data || {};
+	const {
+		_id,
+		name,
+		email,
+		emailVerified,
+		role,
+		img,
+		createdAt,
+		updatedAt,
+		pkg,
+	} = data || {};
 
-    // Save the user data to the store
-    store.dispatch(
-        addUserData({
-            _id,
-            name,
-            email,
-            emailVerified,
-            role,
-            img,
-            createdAt,
-            pkg,
-        })
-    );
+	// Save the user data to the store
+	store.dispatch(
+		addUserData({
+			_id,
+			name,
+			email,
+			emailVerified,
+			role,
+			img,
+			createdAt,
+			updatedAt,
+			pkg,
+		})
+	);
 }
