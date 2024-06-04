d9adf47fcdc15b1ddb9e612a92982b7d267a2b56
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-06-04 15:18:57 +0600

    update: made the User component responsive

--- src/components/Profile/User/User.jsx
+++ src/components/Profile/User/User.jsx
@@ -8,17 +8,17 @@ export default function User() {
 	const { name, img } = useSelector((state) => state.user);
 
 	return (
-		<section className='w-4/5 mx-auto'>
+		<section className='w-full xl:w-4/5 mx-auto'>
 			{/* Header */}
-			<div className='pb-8 flex items-center gap-x-6'>
+			<div className='w-full pb-8 flex items-center gap-x-6'>
 				<img
 					src={img ? img : modelImg}
-					alt=''
-					className='size-28 aspect-square ml-4 object-cover rounded-full relative z-20'
+					alt='User Image'
+					className='size-20 md:size-28 aspect-square ml-4 object-cover rounded-full relative z-20'
 				/>
 				<div className='font-Popins'>
-					<p>Hello</p>
-					<h3 className='text-2xl'>{name}</h3>
+					<p className='text-lg'>Hello</p>
+					<h3 className='text-xl md:text-2xl'>{name}</h3>
 				</div>
 			</div>
 
