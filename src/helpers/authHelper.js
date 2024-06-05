302a2a3fca92f84592bc4d1ea58410f77e4191ef
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-06-05 13:35:53 +0600

    update: removed showing notification functionality inside verifyEmailAddress function and added formatFirebaseError function into index.js file

--- src/helpers/authHelper.js
+++ src/helpers/authHelper.js
@@ -7,7 +7,6 @@ import {
 	signInWithPopup,
 } from 'firebase/auth';
 import app from '../../firebase.config';
-import showNotification from './showNotification';
 
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
@@ -24,18 +23,12 @@ const signInWithPassword = (email, password) =>
 	signInWithEmailAndPassword(auth, email, password);
 
 // Send email to verify user email
-const verifyEmailAddress = () => {
+const verifyEmailAddress = async () => {
 	const user = auth.currentUser;
 
 	const url = `https://mad-chef-server.vercel.app/users/user/verify-email?uid=${user.uid}`;
 
-	return showNotification('promise', 'Sending verification email', {
-		promise: sendEmailVerification(user, {
-			url,
-		}),
-		successMessage: 'Message successfully sent',
-		errorMessage: 'An error occurred while sending the verification email',
-	});
+	return sendEmailVerification(user, { url });
 };
 
 export {
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,5 +1,6 @@
 export { default as perseDate } from './perseDate';
 export { default as convertTo12HourFormat } from './convertTo12HourFormat';
+export { default as formatFirebaseError } from './formatFirebaseError';
 export { default as removeNotifications } from './removeNotifications';
 export { default as showNotification } from './showNotification';
 export { default as storeUserData } from './storeUserData';
