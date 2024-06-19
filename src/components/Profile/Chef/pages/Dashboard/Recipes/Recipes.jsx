fd82707b033e4c29ce8330a7598be678d2ebdec2
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-06-19 20:31:17 +0600

    update: completed the Recipes page and made it responsive

--- src/components/Profile/Chef/pages/Dashboard/Recipes/Recipes.jsx
+++ src/components/Profile/Chef/pages/Dashboard/Recipes/Recipes.jsx
@@ -1,7 +1,57 @@
+import { Helmet } from 'react-helmet-async';
+import { useSelector } from 'react-redux';
+import { NoContent, Pagination, Recipe } from '../../../../../../shared';
+import { useGetRecipesQuery } from '../../../../../../features/recipe/recipeApi';
+
 export default function Recipes() {
-    return (
-        <section>
-            Recipes
-        </section>
-    );
+	const { _id } = useSelector((state) => state.user);
+
+	const { data, isSuccess, isError, error } = useGetRecipesQuery({
+		data_filter: {
+			chefId: _id,
+		},
+		include: '_id,img,title,ingredients,rating',
+	});
+
+	const { data: recipes, meta } = data || {};
+
+	// Get the page details
+	const pageDetails = meta?.page?.split('/');
+	const currPage = pageDetails?.length && parseInt(pageDetails[0]);
+	const totalPages = pageDetails?.length && parseInt(pageDetails[1]);
+
+	let content;
+	if (isSuccess && recipes?.length === 0) {
+		content = <NoContent />;
+	} else if (isError) {
+		content = (
+			<p className='w-fit mt-10 p-3 bg-red-300 text-lg text-red-700 font-semibold rounded'>
+				{error?.data?.error}
+			</p>
+		);
+	} else if (isSuccess && recipes?.length > 0) {
+		content = (
+			<>
+				{recipes.map((recipe, index) => (
+					<Recipe key={index} recipe={recipe} />
+				))}
+
+				<Pagination activePage={currPage} totalPages={totalPages} />
+			</>
+		);
+	}
+
+	return (
+		<section className='w-full my-5 px-2 md:px-5'>
+			<Helmet>
+				<title>Recipes | Profile - Mad Chef</title>
+			</Helmet>
+
+			<h3 className='w-3/4 md:w-1/2 mb-5 px-2 border-b-2 text-2xl font-semibold text-slate-700 border-Primary'>
+				My Recipes:
+			</h3>
+
+			{content}
+		</section>
+	);
 }
