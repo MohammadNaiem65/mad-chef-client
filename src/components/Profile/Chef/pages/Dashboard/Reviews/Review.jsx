ab282dff039a7adc4befd2ecefe580e2691f187a
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-06-24 07:30:24 +0600

    update: completed the Reviews page design and made it responsive

---
+++ src/components/Profile/Chef/pages/Dashboard/Reviews/Review.jsx
@@ -0,0 +1,47 @@
+import { useGetUserDataQuery } from '../../../../../../features/user/userApi';
+import { Avatar, Rating } from '../../../../../../shared';
+
+export default function Review({ review }) {
+	const { userId, rating, message, createdAt, updatedAt } = review || {};
+
+	const { data } = useGetUserDataQuery({ userId, include: 'name,img' });
+	const { name, img } = data?.data || {};
+
+	return (
+		<div className='w-full lg:w-[26rem] mx-auto lg:mx-0'>
+			<div className='flex border-2 rounded-lg border-gray-200 border-opacity-50 px-5 py-4 md:p-8 sm:flex-row flex-col hover:shadow-xl duration-300'>
+				{/* Conditional rendering of userImg or Avatar */}
+				{img ? (
+					<img
+						src={img}
+						alt={name}
+						className='w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full flex-shrink-0'
+					/>
+				) : (
+					<div className='size-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0 overflow-hidden'>
+						<Avatar />
+					</div>
+				)}
+
+				<div className='flex-grow'>
+					<h2
+						title={name}
+						className='w-full lg:w-64 text-gray-900 text-lg font-medium truncate'>
+						{name}
+					</h2>
+					<div className='mb-1 text-yellow-500 flex gap-x-1'>
+						{<Rating rating={rating} />}
+					</div>
+
+					{createdAt !== updatedAt && (
+						<p className='mb-1 text-gray-500 text-xs'>Edited</p>
+					)}
+
+					<p className='w-full lg:w-64 mt-3 leading-relaxed text-justify text-gray-400 line-clamp-3 group-hover:line-clamp-none'>
+						{message}
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+}
--- src/components/Profile/Chef/pages/Dashboard/Reviews/Reviews.jsx
+++ src/components/Profile/Chef/pages/Dashboard/Reviews/Reviews.jsx
@@ -1,7 +1,51 @@
+import { useSelector } from 'react-redux';
+import { useGetChefReviewsQuery } from '../../../../../../features/chef/chefApi';
+import { Error, NoContent, Spinner } from '../../../../../../shared';
+import Review from './Review';
+import { Helmet } from 'react-helmet-async';
+
 export default function Reviews() {
-    return (
-        <section>
-            Reviews
-        </section>
-    );
+	const { _id } = useSelector((state) => state.user);
+
+	const { data, isLoading, isSuccess, isError, error } =
+		useGetChefReviewsQuery({
+			chef_id: _id,
+			sort: 'createdAt,updatedAt',
+		});
+
+	const { data: reviews } = data || {};
+
+	let content;
+
+	// Decide what to render
+	if (isLoading) {
+		content = <Spinner />;
+	} else if (isError) {
+		content = <Error message={error?.message} />;
+	} else if (isSuccess && reviews?.length === 0) {
+		content = <NoContent message='You have not made any reviews yet.' />;
+	} else if (isSuccess && reviews?.length > 0) {
+		content = reviews.map((review) => (
+			<Review key={review._id} review={review} />
+		));
+	}
+
+	return (
+		<section className='w-full my-5 px-2 md:px-5'>
+			<Helmet>
+				<title>Recipes | Profile - Mad Chef</title>
+			</Helmet>
+
+			<h3 className='w-3/4 md:w-1/2 mb-5 px-2 border-b-2 text-2xl font-semibold text-slate-700 border-Primary'>
+				My Reviews:
+			</h3>
+
+			<div
+				className={`w-full h-[20rem] my-4 px-2 md:pr-3 pb-10 flex flex-wrap gap-3 Bottom-Shadow ${
+					reviews?.length > 2 && 'overflow-y-scroll'
+				}`}>
+				{content}
+			</div>
+		</section>
+	);
 }
