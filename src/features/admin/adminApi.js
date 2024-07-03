cc245f3781420d0ad40a7e7d48c5ff33d916d6e9
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-07-03 17:21:29 +0600

    add: created adminApi to handle admin related api and getAdminData query to fetch admin data

---
+++ src/features/admin/adminApi.js
@@ -0,0 +1,16 @@
+import apiSlice from '../api/apiSlice';
+
+const adminApi = apiSlice.injectEndpoints({
+	endpoints: (builder) => ({
+		getAdminData: builder.query({
+			query: ({id}) => ({
+				url: `/admins/admin/${id}`,
+				method: 'GET',
+			}),
+			transformResponse: (response) => response.data,
+		}),
+	}),
+});
+
+export default adminApi;
+export const { useGetAdminDataQuery } = adminApi;
