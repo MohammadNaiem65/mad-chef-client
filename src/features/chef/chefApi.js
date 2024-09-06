e21a38f372397079acf4ac9fa421d0d3011938f3
Md Rifat Ahmed Tusar <mohammadnaiem661065@gmail.com>
2024-09-06 22:28:07 +0600

    add: created useGetRolePromotionApplicationQuery and useGetRolePromotionApplicationsQuery query

--- src/features/chef/chefApi.js
+++ src/features/chef/chefApi.js
@@ -1,123 +1,131 @@
 import apiSlice from '../api/apiSlice';
 
 const chefApi = apiSlice.injectEndpoints({
-	endpoints: (builder) => ({
-		getChefs: builder.query({
-			query: ({ page, limit, sort, order, include, exclude }) => {
-				// Define the base URL
-				const baseUrl = '/chefs';
+    endpoints: (builder) => ({
+        getChefs: builder.query({
+            query: ({ page, limit, sort, order, include, exclude }) => {
+                // Define the base URL
+                const baseUrl = '/chefs';
 
-				// Create an object with all parameters
-				const params = { page, limit, sort, order, include, exclude };
+                // Create an object with all parameters
+                const params = { page, limit, sort, order, include, exclude };
 
-				// Use reduce to construct the query string
-				const queryString = Object.entries(params)
-					.reduce((acc, [key, value]) => {
-						// Only include parameters that have a value
-						if (value !== undefined) {
-							acc.push(`${key}=${encodeURIComponent(value)}`);
-						}
-						return acc;
-					}, [])
-					.join('&');
+                // Use reduce to construct the query string
+                const queryString = Object.entries(params)
+                    .reduce((acc, [key, value]) => {
+                        // Only include parameters that have a value
+                        if (value !== undefined) {
+                            acc.push(`${key}=${encodeURIComponent(value)}`);
+                        }
+                        return acc;
+                    }, [])
+                    .join('&');
 
-				// Construct the final URL
-				const url = queryString ? `${baseUrl}?${queryString}` : baseUrl;
+                // Construct the final URL
+                const url = queryString ? `${baseUrl}?${queryString}` : baseUrl;
 
-				return { url };
-			},
+                return { url };
+            },
 
-			serializeQueryArgs: ({ limit, sort, order, include, exclude }) => {
-				// To merge previous cache, ignore page number
-				return {
-					page: 'all', // Use 'all' to ignore page number
-					limit,
-					sort,
-					order,
-					include,
-					exclude,
-				};
-			},
+            serializeQueryArgs: ({ queryArgs }) => {
+                const { page, limit, sort, order, include, exclude, merge } =
+                    queryArgs;
 
-			// Merge the incoming data with the existing data
-			merge: (existingResponse, incomingResponse) => {
-				incomingResponse.data = [
-					...existingResponse.data,
-					...incomingResponse.data,
-				];
+                const args = { limit, sort, order, include, exclude };
 
-				return incomingResponse;
-			},
+                // If merge is true, ignore page number to merge previous cache
+                if (merge) {
+                    args.page = 'all'; // Use 'all' to ignore page number
+                } else {
+                    args.page = page;
+                }
 
-			// force to refetch if query argument (page) changes
-			forceRefetch: ({ currentArg, previousArg }) => {
-				return (
-					currentArg?.page !== undefined &&
-					previousArg?.page !== currentArg?.page
-				);
-			},
-		}),
-		getChef: builder.query({
-			query: ({ chef_id, include, exclude }) => {
-				const baseUrl = `/chefs/chef/${chef_id}`;
-				const params = { include, exclude };
+                return args;
+            },
 
-				// Use reduce to construct the query string
-				const queryString = Object.entries(params)
-					.reduce((acc, [key, value]) => {
-						// Only include parameters that have a value
-						if (value) {
-							acc.push(`${key}=${encodeURIComponent(value)}`);
-						}
-						return acc;
-					}, [])
-					.join('&');
+            // Merge the incoming data with the existing data
+            merge: (existingResponse, incomingResponse, { arg }) => {
+                const { merge } = arg || false;
 
-				// Construct the URL without filter parameters
-				const url = queryString ? `${baseUrl}?${queryString}` : baseUrl;
+                if (merge) {
+                    incomingResponse.data = [
+                        ...existingResponse.data,
+                        ...incomingResponse.data,
+                    ];
+                }
 
-				return { url };
-			},
-		}),
-		getChefReviews: builder.query({
-			query: ({ chef_id, page, limit, sort, order }) => {
-				// Define the base URL
-				const baseUrl = `/chefs/chef/${chef_id}/reviews`;
+                return incomingResponse;
+            },
 
-				// Create an object with all parameters
-				const params = { page, limit, sort, order };
+            // force to refetch if query argument (page) changes
+            forceRefetch: ({ currentArg, previousArg }) => {
+                return (
+                    currentArg?.page !== undefined &&
+                    previousArg?.page !== currentArg?.page
+                );
+            },
+        }),
+        getChef: builder.query({
+            query: ({ chef_id, include, exclude }) => {
+                const baseUrl = `/chefs/chef/${chef_id}`;
+                const params = { include, exclude };
 
-				// Use reduce to construct the query string
-				const queryString = Object.entries(params)
-					.reduce((acc, [key, value]) => {
-						// Only include parameters that have a value
-						if (value !== undefined) {
-							acc.push(`${key}=${encodeURIComponent(value)}`);
-						}
-						return acc;
-					}, [])
-					.join('&');
+                // Use reduce to construct the query string
+                const queryString = Object.entries(params)
+                    .reduce((acc, [key, value]) => {
+                        // Only include parameters that have a value
+                        if (value) {
+                            acc.push(`${key}=${encodeURIComponent(value)}`);
+                        }
+                        return acc;
+                    }, [])
+                    .join('&');
 
-				// Construct the final URL
-				const url = queryString ? `${baseUrl}?${queryString}` : baseUrl;
+                // Construct the URL without filter parameters
+                const url = queryString ? `${baseUrl}?${queryString}` : baseUrl;
 
-				return { url };
-			},
-		}),
-		addChefReview: builder.mutation({
-			query: ({ chef_id, data }) => ({
-				url: `/chefs/chef/${chef_id}/reviews`,
-				method: 'POST',
-				body: data,
-			}),
-		}),
-	}),
+                return { url };
+            },
+        }),
+        getChefReviews: builder.query({
+            query: ({ chef_id, page, limit, sort, order }) => {
+                // Define the base URL
+                const baseUrl = `/chefs/chef/${chef_id}/reviews`;
+
+                // Create an object with all parameters
+                const params = { page, limit, sort, order };
+
+                // Use reduce to construct the query string
+                const queryString = Object.entries(params)
+                    .reduce((acc, [key, value]) => {
+                        // Only include parameters that have a value
+                        if (value !== undefined) {
+                            acc.push(`${key}=${encodeURIComponent(value)}`);
+                        }
+                        return acc;
+                    }, [])
+                    .join('&');
+
+                // Construct the final URL
+                const url = queryString ? `${baseUrl}?${queryString}` : baseUrl;
+
+                return { url };
+            },
+        }),
+        addChefReview: builder.mutation({
+            query: ({ chef_id, data }) => ({
+                url: `/chefs/chef/${chef_id}/reviews`,
+                method: 'POST',
+                body: data,
+            }),
+        }),
+    }),
 });
 
 export default chefApi;
 export const {
-	useGetChefsQuery,
-	useGetChefQuery,
-	useGetChefReviewsQuery,
-	useAddChefReviewMutation,
+    useGetChefsQuery,
+    useGetChefQuery,
+    useGetChefReviewsQuery,
+    useAddChefReviewMutation,
 } = chefApi;
--- src/features/role/roleApi.js
+++ src/features/role/roleApi.js
@@ -1,24 +1,60 @@
 import apiSlice from '../api/apiSlice';
 
 const roleApi = apiSlice.injectEndpoints({
-	endpoints: (builder) => ({
-		applyForPromotion: builder.mutation({
-			query: ({ role }) => ({
-				url: `/roles/apply-for-promotion?role=${role}`,
-				method: 'POST',
-			}),
-		}),
-		checkApplicationForPromotion: builder.query({
-			query: ({ role }) => ({
-				url: `/roles/has-applied-for-promotion?role=${role}`,
-				method: 'GET',
-			}),
-		}),
-	}),
+    endpoints: (builder) => ({
+        applyForPromotion: builder.mutation({
+            query: ({ role }) => ({
+                url: `/roles/apply-for-promotion?role=${role}`,
+                method: 'POST',
+            }),
+        }),
+        checkApplicationForPromotion: builder.query({
+            query: ({ role }) => ({
+                url: `/roles/has-applied-for-promotion?role=${role}`,
+                method: 'GET',
+            }),
+        }),
+        getRolePromotionApplication: builder.query({
+            query: ({ applicationId }) => ({
+                url: `/roles/role-promotion-application/${applicationId}`,
+                method: 'GET',
+            }),
+        }),
+        getRolePromotionApplications: builder.query({
+            query: ({ page, limit, sort, order }) => {
+                // Define the base URL
+                const baseUrl = '/roles/role-promotion-applications';
+
+                // Create an object with required parameters
+                const params = {
+                    page,
+                    limit,
+                    sort,
+                    order,
+                };
+
+                // Use reduce to construct the query string without filter parameter
+                const queryString = Object.entries(params)
+                    .reduce((acc, [key, value]) => {
+                        // Only include parameters that have a value
+                        if (value) {
+                            acc.push(`${key}=${encodeURIComponent(value)}`);
+                        }
+                        return acc;
+                    }, [])
+                    .join('&');
+
+                // return the object
+                return { url: `${baseUrl}?${queryString}` };
+            },
+        }),
+    }),
 });
 
 export default roleApi;
 export const {
-	useApplyForPromotionMutation,
-	useCheckApplicationForPromotionQuery,
+    useApplyForPromotionMutation,
+    useCheckApplicationForPromotionQuery,
+    useGetRolePromotionApplicationQuery,
+    useGetRolePromotionApplicationsQuery,
 } = roleApi;
