d673ce9d7a2d220707ee79d3340b96e3d5dda4d2
Md Rifat Ahmed Tusar <mohammadnaiem661065@gmail.com>
2024-09-20 01:05:34 +0600

    update: made the Error component support className for more flexibility

--- src/shared/Error.jsx
+++ src/shared/Error.jsx
@@ -1,9 +1,11 @@
-export default function Error({ message }) {
-	return (
-		<p className='mt-6 p-2 font-semibold text-red-700 text-center bg-red-200 rounded-sm'>
-			{message
-				? message
-				: 'Something went wrong. Please try again later.'}
-		</p>
-	);
+export default function Error({ message, className }) {
+    return (
+        <p
+            className={`mt-6 p-2 font-semibold text-red-700 text-center bg-red-200 rounded-sm ${className}`}
+        >
+            {message
+                ? message
+                : 'Something went wrong. Please try again later.'}
+        </p>
+    );
 }
