89aedda1791c607020ca9d42a23610d39bef8dab
Md Rifat Ahmed Tusar <mohammadnaiem661065@gmail.com>
2024-09-19 20:51:25 +0600

    fix: made the ChefRecipes code more clean and fixed pagination bug

--- src/components/Recipes/ChefRecipes.jsx
+++ src/components/Recipes/ChefRecipes.jsx
@@ -1,97 +1,78 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa6';
 
 import { useGetRecipesQuery } from '../../features/recipe/recipeApi';
 import Recipe from '../../shared/Recipe';
 import { Pagination } from '../../shared';
 import { useSelector } from 'react-redux';
+import { usePaginationInfo } from '../../hooks';
 
 export default function ChefRecipes({ chefId }) {
-	const [pageDetails, setPageDetails] = useState({
-		currPage: 1,
-		totalPages: null,
-	});
-	const recipeFilter = useSelector((state) => state.recipeFilter);
+    const [currPage, setCurrPage] = useState(1);
 
-	const { data } = useGetRecipesQuery({
-		data_filter: {
-			chefId,
-			searchQuery: recipeFilter.keyword,
-			uploadDate: recipeFilter.uploadDate,
-			region: recipeFilter.region,
-		},
-		sort: recipeFilter.sortBy,
-		order: recipeFilter.order,
-		page: pageDetails.currPage,
-		include: 'title,ingredients,rating,img',
-	});
-	const { data: recipes, meta } = data || {};
+    const recipeFilter = useSelector((state) => state.recipeFilter);
 
-	useEffect(() => {
-		const pNumber = meta?.page?.split('/');
+    const { data, isLoading } = useGetRecipesQuery({
+        data_filter: {
+            chefId,
+            searchQuery: recipeFilter.keyword,
+            uploadDate: recipeFilter.uploadDate,
+            region: recipeFilter.region,
+        },
+        sort: recipeFilter.sortBy,
+        order: recipeFilter.order,
+        page: currPage,
+        include: 'title,ingredients,rating,img',
+    });
+    const { data: recipes, meta } = data || {};
 
-		if (pNumber?.length > 0) {
-			setPageDetails({
-				currPage: parseInt(pNumber[0]),
-				totalPages: parseInt(pNumber[1]),
-			});
-		}
-	}, [recipes, meta?.page]);
+    const { activePage, totalPages } = usePaginationInfo(meta?.page);
 
-	return (
-		<section className='w-full px-5 lg:p-0'>
-			{!recipes?.length ? (
-				<p className='h-full mt-36 text-slate-700 text-center font-semibold'>
-					No Content Found
-				</p>
-			) : (
-				<div className='flex justify-between items-center'>
-					<h3 className='text-xl font-semibold font-Vollkorn'>
-						Recipes:
-					</h3>
-					<div className='text-3xl text-Primary flex items-center gap-x-2'>
-						<button
-							disabled={pageDetails.currPage === 1}
-							className='disabled:translate-y-1 disabled:scale-95 duration-300 disabled:text-blue-700 translate-y-0'
-							onClick={() =>
-								setPageDetails((prev) => ({
-									...prev,
-									currPage: prev.currPage - 1,
-								}))
-							}>
-							<FaAngleLeft className='cursor-pointer' />
-						</button>
-						<button
-							disabled={
-								pageDetails.currPage ===
-									pageDetails.totalPages ||
-								pageDetails.currPage === null
-							}
-							className='disabled:translate-y-1 disabled:scale-95 duration-300 disabled:text-blue-700 translate-y-0'
-							onClick={() =>
-								setPageDetails((prev) => ({
-									...prev,
-									currPage: prev.currPage + 1,
-								}))
-							}>
-							<FaAngleRight className='cursor-pointer' />
-						</button>
-					</div>
-				</div>
-			)}
+    return (
+        <section className='w-full px-5 lg:p-0'>
+            {!recipes?.length ? (
+                <p className='h-full mt-36 text-slate-700 text-center font-semibold'>
+                    No Content Found
+                </p>
+            ) : (
+                <div className='flex justify-between items-center'>
+                    <h3 className='text-xl font-semibold font-Vollkorn'>
+                        Recipes:
+                    </h3>
+                    <div className='text-3xl text-Primary flex items-center gap-x-2'>
+                        <button
+                            disabled={currPage === 1}
+                            className='disabled:translate-y-1 disabled:scale-95 duration-300 disabled:text-blue-700 translate-y-0'
+                            onClick={() => setCurrPage((prev) => prev - 1)}
+                        >
+                            <FaAngleLeft className='cursor-pointer' />
+                        </button>
+                        <button
+                            disabled={
+                                currPage === totalPages || totalPages === 1
+                            }
+                            className='disabled:translate-y-1 disabled:scale-95 duration-300 disabled:text-blue-700 translate-y-0'
+                            onClick={() => setCurrPage((prev) => prev + 1)}
+                        >
+                            <FaAngleRight className='cursor-pointer' />
+                        </button>
+                    </div>
+                </div>
+            )}
 
-			<section className='mt-4'>
-				{recipes?.map((recipe) => (
-					<Recipe key={recipe._id} recipe={recipe} />
-				))}
-			</section>
+            <section className='mt-4'>
+                {recipes?.map((recipe) => (
+                    <Recipe key={recipe._id} recipe={recipe} />
+                ))}
+            </section>
 
-			{recipes?.length > 0 && (
-				<Pagination
-					activePage={pageDetails.currPage}
-					totalPages={pageDetails.totalPages}
-				/>
-			)}
-		</section>
-	);
+            {!isLoading && totalPages > 1 && (
+                <Pagination
+                    activePage={activePage}
+                    totalPages={totalPages}
+                    setCurrPage={setCurrPage}
+                />
+            )}
+        </section>
+    );
 }
