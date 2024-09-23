026945d675a2923da8fdfca5c1c43f6205f9ca38
Md Rifat Ahmed Tusar <mohammadnaiem661065@gmail.com>
2024-09-23 18:06:21 +0600

    fix: added pagination into Recipe Ratings subpage of Dashboard page

--- src/components/Profile/User/pages/Dashboard/Ratings.jsx
+++ src/components/Profile/User/pages/Dashboard/Ratings.jsx
@@ -1,57 +1,68 @@
 import { useSelector } from 'react-redux';
 import { Helmet } from 'react-helmet-async';
 import { useGetRecipeRatingsQuery } from '../../../../../features/recipe/recipeApi';
-import { NoContent, Spinner } from '../../../../../shared';
+import { NoContent, Pagination, Spinner } from '../../../../../shared';
 import RatingCardForRecipeRating from './RatingCardForRecipeRating';
+import usePaginationInfo from '../../../../../hooks/usePaginationInfo';
 
 export default function Ratings() {
-	const { _id: userId } = useSelector((state) => state.user);
-	const { data, isLoading, isSuccess, isError, error } =
-		useGetRecipeRatingsQuery({ data_filter: { userId } });
+    const { _id: userId } = useSelector((state) => state.user);
+    const { data, isLoading, isSuccess, isError, error } =
+        useGetRecipeRatingsQuery({ data_filter: { userId } });
 
-	const { data: ratings } = data || {};
+    const { data: ratings, meta } = data || {};
 
-	let content;
-	if (isLoading) {
-		content = <Spinner />;
-	} else if (isSuccess && ratings?.length === 0) {
-		content = (
-			<section className='my-28 md:mx-auto px-5'>
-				<NoContent />
-			</section>
-		);
-	} else if (isError) {
-		content = (
-			<p className='w-fit mt-10 p-3 bg-red-300 text-lg text-red-700 font-semibold rounded'>
-				{error?.data}
-			</p>
-		);
-	} else if (isSuccess && ratings?.length > 0) {
-		content = (
-			<>
-				{ratings.map((rating) => (
-					<RatingCardForRecipeRating
-						key={rating?._id}
-						userId={userId}
-						rating={rating}
-					/>
-				))}
-			</>
-		);
-	}
+    const { activePage, totalPages } = usePaginationInfo(meta?.page);
 
-	return (
-		<section className='px-2 text-gray-600'>
-			<Helmet>
-				<title>Recipe Ratings | Profile - Mad Chef</title>
-			</Helmet>
+    let content;
+    if (isLoading) {
+        content = <Spinner />;
+    } else if (isSuccess && ratings?.length === 0) {
+        content = (
+            <section className='my-28 md:mx-auto px-5'>
+                <NoContent />
+            </section>
+        );
+    } else if (isError) {
+        content = (
+            <p className='w-fit mt-10 p-3 bg-red-300 text-lg text-red-700 font-semibold rounded'>
+                {error?.data}
+            </p>
+        );
+    } else if (isSuccess && ratings?.length > 0) {
+        content = (
+            <>
+                {ratings.map((rating) => (
+                    <RatingCardForRecipeRating
+                        key={rating?._id}
+                        userId={userId}
+                        rating={rating}
+                    />
+                ))}
+            </>
+        );
+    }
 
-			<div
-				className={`w-full h-[20rem] my-4 px-2 md:pr-3 flex flex-wrap gap-3 ${
-					ratings?.length > 2 && 'overflow-y-scroll'
-				}`}>
-				{content}
-			</div>
-		</section>
-	);
+    return (
+        <section className='px-2 text-gray-600'>
+            <Helmet>
+                <title>Recipe Ratings | Profile - Mad Chef</title>
+            </Helmet>
+
+            <div
+                className={`w-full h-[20rem] my-4 px-2 md:pr-3 flex flex-wrap gap-3 ${
+                    ratings?.length > 2 && 'overflow-y-scroll'
+                }`}
+            >
+                {content}
+
+                {totalPages > 1 && (
+                    <Pagination
+                        activePage={activePage}
+                        totalPages={totalPages}
+                    />
+                )}
+            </div>
+        </section>
+    );
 }
