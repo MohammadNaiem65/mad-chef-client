f8ff61cd42730bebacea9ebc1795c5f91815d680
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-10-10 00:28:18 +0600

    update: made the Rating component clickable and set rating

--- src/shared/Rating.jsx
+++ src/shared/Rating.jsx
@@ -1,13 +1,29 @@
 import { FaRegStar, FaStar, FaStarHalfAlt } from 'react-icons/fa';
 
-export default function Rating({ rating }) {
-	return Array.from({ length: 5 }, (_, i) => {
-		if (rating >= i + 1) {
-			return <FaStar key={i} />;
-		} else if (rating >= i + 0.5) {
-			return <FaStarHalfAlt key={i} />;
-		} else {
-			return <FaRegStar key={i} />;
-		}
-	});
+/**
+ * A rating component that displays a certain number of stars based on the rating value.
+ * Each star can be clicked to set the rating to that value.
+ * @param {Object} props
+ * @prop {number} rating The current rating, a value between 0 and 5.
+ * @prop {function} setRating An optional function to set the rating, takes a number as argument.
+ * @returns {JSX.Element} A JSX element representing the rating stars.
+ */
+export default function Rating({ rating, setRating }) {
+    const handleSetRating = (value) => {
+        if (typeof setRating === 'function') {
+            setRating(value);
+        }
+    };
+
+    return Array.from({ length: 5 }, (_, i) => {
+        if (rating >= i + 1) {
+            return <FaStar key={i} onClick={() => handleSetRating(i + 1)} />;
+        } else if (rating >= i + 0.5) {
+            return (
+                <FaStarHalfAlt key={i} onClick={() => handleSetRating(i + 1)} />
+            );
+        } else {
+            return <FaRegStar key={i} onClick={() => handleSetRating(i + 1)} />;
+        }
+    });
 }
