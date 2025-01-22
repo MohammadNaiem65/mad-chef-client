f56d811a1f00ec63900ea5dd9961419edb973d73
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-01-22 23:23:19 +0600

    update: completed Messages page design for student page

---
+++ src/components/Profile/Student/pages/Messages/Messages.jsx
@@ -0,0 +1,31 @@
+import { Suspense, useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+import { Spinner } from '../../../../../shared';
+import Conversations from './Conversations';
+
+export default function Messages() {
+    useEffect(() => {
+        window.scrollTo(0, 130);
+    }, []);
+
+    return (
+        <>
+            <Helmet>
+                <title>Messages | Profile - Mad Chef</title>
+            </Helmet>
+
+            <section className='w-full min-h-[24.5rem] border-b border-gray-300 flex'>
+                {/* Conversations */}
+                <Conversations />
+
+                {/* Chat */}
+                <section className='flex-grow'>
+                    <Suspense fallback={<Spinner />}>
+                        <Outlet />
+                    </Suspense>
+                </section>
+            </section>
+        </>
+    );
+}
